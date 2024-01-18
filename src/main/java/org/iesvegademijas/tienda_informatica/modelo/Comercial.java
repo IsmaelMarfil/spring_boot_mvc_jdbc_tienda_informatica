b397982a5cb6776bdcb2644e5464b650b54e63c8
Ismael <imarram1104@g.educaand.es>
2024-01-18 12:57:22 +0100

    VA TODO

--- src/main/java/org/iesvegademijas/tienda_informatica/modelo/Comercial.java
+++ src/main/java/org/iesvegademijas/tienda_informatica/modelo/Comercial.java
@@ -5,19 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comercial {
     private int id;
     private String nombre;
     private String apellido1;
     private String apellido2;
     private float comisión;
-
-    public Comercial(int id, String nombre, String apellido1, String apellido2, float comisión) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido1 = apellido1;
-        this.apellido2 = apellido2;
-        this.comisión = comisión;
-    }
 }
+
+
 
