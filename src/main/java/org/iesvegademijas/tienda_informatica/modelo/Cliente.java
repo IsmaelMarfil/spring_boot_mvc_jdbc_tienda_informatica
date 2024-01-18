1ca3b0c031773bde04f1f8044081ef2fbdb6fdfa
Ismael <imarram1104@g.educaand.es>
2024-01-18 20:23:11 +0100

    Hecho

---
+++ src/main/java/org/iesvegademijas/tienda_informatica/modelo/Cliente.java
@@ -0,0 +1,20 @@
+package org.iesvegademijas.tienda_informatica.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String ciudad;
+    private int categoría;
+}
+
+
+
