9d9c7f52e2868fb79f8825a7309f15a26a75d3db
Ismael <imarram1104@g.educaand.es>
2024-01-29 12:10:34 +0100

    mapper y dto hechos

--- src/main/java/org/iesvegademijas/tienda_informatica/mapper/PedidoMapper.java
+++ src/main/java/org/iesvegademijas/tienda_informatica/mapper/PedidoMapper.java
@@ -8,12 +8,12 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface PedidoMapper {
 
-    @Mapping(source = "idCliente", target = "idCliente")
-    @Mapping(source = "idComercial", target = "idComercial")
-    public PedidoFormDTO pedidoAPedidoFormDTO(Pedido pedido);
+    //@Mapping(source = "idCliente", target = "idCliente")
+    //@Mapping(source = "idComercial", target = "idComercial")
+    //public PedidoFormDTO pedidoAPedidoFormDTO(Pedido pedido);
 
-    @Mapping(source = "idCliente", target = "idCliente")
-    @Mapping(source = "idComercial", target = "idCliente")
-    public Pedido pedidoFormDTOAPedido(PedidoFormDTO pedidoFormDTO);
+    //@Mapping(source = "idCliente", target = "idCliente")
+    //@Mapping(source = "idComercial", target = "idCliente")
+    //public Pedido pedidoFormDTOAPedido(PedidoFormDTO pedidoFormDTO);
 
 }
